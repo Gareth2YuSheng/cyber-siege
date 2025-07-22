f8a9d8cd3ccb45f05c5df913a2014606c1a9351e
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2025-07-22 22:59:02 +0800

    Update CrossPlatformScrollScript.cs

--- Cyber Siege/Assets/Scripts/UI/CrossPlatformScrollScript.cs
+++ Cyber Siege/Assets/Scripts/UI/CrossPlatformScrollScript.cs
@@ -5,19 +5,24 @@ using UnityEngine.UI;
 public class CrossPlatformScrollScript : ScrollRect
 {
     [Header("Attributes")]
-    [SerializeField] private float macSensitivityMultiplier = 1f;
-    [SerializeField] private float windowsSensitivityMultiplier = 5f;
+    // public float macSensitivityMultiplier = 1f;
+    // public float windowsSensitivityMultiplier = 5f;
+
+    public float windowsSensMultiplier = 7f;
+    public float macSensMultiplier = 1f;
 
     public override void OnScroll(PointerEventData data)
     {
-        float sensitivityModifier = windowsSensitivityMultiplier;
+        float sensitivityModifier = windowsSensMultiplier;
 
 #if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
-        sensitivityModifier = macSensitivityMultiplier;
+        sensitivityModifier = macSensMultiplier;
 #endif
 
         // Modify scrollDelta in place before calling base
         data.scrollDelta = new Vector2(0, data.scrollDelta.y);
+
+        data.scrollDelta *= sensitivityModifier;
 
         base.OnScroll(data);
     }
